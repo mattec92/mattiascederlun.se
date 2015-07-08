6a38d1a845df7eda8822053cdc15b36dc59aae89
Mattias Cederlund <mattec92@hotmail.com>
2015-07-08 16:10:34 +0200

    Basic Google Analytics coverage.

---
+++ analytics_operation.js
@@ -0,0 +1,15 @@
+$('#linkedinLink').on('click', function() {
+    ga('send', 'event', 'link', 'click', 'linkedinLink');
+});
+
+$('#emailLink').on('click', function() {
+    ga('send', 'event', 'link', 'click', 'emailLink');
+});
+
+$('#phoniacLink').on('click', function() {
+    ga('send', 'event', 'link', 'click', 'phoniacLink');
+});
+
+$('#githubLink').on('click', function() {
+    ga('send', 'event', 'link', 'click', 'githubLink');
+});
---
+++ analytics_setup.js
@@ -0,0 +1,44 @@
+var DEBUG = false;
+var TRACKING_ID = 'UA-49525518-3';
+
+if (DEBUG) {
+    window.ga_debug = {
+        trace: true
+    };
+
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'http://www.google-analytics.com/analytics_debug.js', 'ga');
+
+    ga('create',
+        TRACKING_ID, {
+            'cookieDomain': 'none'
+        });
+}
+else {
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', TRACKING_ID, 'auto');
+}
+
+ga('set', 'checkProtocolTask', function() { /* nothing */ });
+ga('set', 'checkStorageTask', function() { /* nothing */ });
+ga('send', 'pageview');
